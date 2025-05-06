fb6a6e80ce5ec011c92a8afe7b02b9f7e7f9695a
Miguel Angelo Bertolo Campos <00001110756926sp@al.educacao.sp.gov.br>
2025-05-06 08:48:35 -0300

    verificação de idade

---
+++ exercicio3.py
@@ -0,0 +1,11 @@
+idade = int(input("Digite sua idade: "))
+nome = input("Digite seu nome: ")
+
+print (f"Seu nome é: {nome}")
+
+print (f"Sua idade é {idade}")
+
+if idade >= 18:
+    print ("Você é maior de idade")
+else:
+    print ("Você é menor de idade")
