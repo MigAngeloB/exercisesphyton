e1de4f96d761b83a425b1ddfa5ca72ffacd12f22
Miguel Angelo Bertolo Campos <00001110756926sp@al.educacao.sp.gov.br>
2025-05-06 11:49:44 -0300

    média de numeros

--- exercicio5.py
+++ exercicio5.py
@@ -1,14 +1,15 @@
-def cadastraraluno(nome,email,serie):
+def cadastraraluno(nome,email,serie,nota,):
     alunos = []
 
     aluno = {
             "nome": nome,
             "email": email,
-            "serie": serie
+            "serie": serie,
+            "nota": nota
     }
 
     alunos.append(aluno)
 
     return alunos
 
-print (cadastraraluno("Miguel", "miguel@gmail.com", "2B"))
+print (cadastraraluno("Miguel", "miguel@gmail.com", "2B", "10"))
