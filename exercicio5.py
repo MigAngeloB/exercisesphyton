50aa3fa0d81eb854ef85f422bf7ede07ce3f5078
Miguel Angelo Bertolo Campos <00001110756926sp@al.educacao.sp.gov.br>
2025-05-06 12:11:32 -0300

    aula dicionario

--- exercicio5.py
+++ exercicio5.py
@@ -1,15 +1,16 @@
-def cadastraraluno(nome,email,serie,nota,):
+def cadastraraluno(nome,email,serie,nota1,nota2,nota3):
     alunos = []
 
     aluno = {
             "nome": nome,
             "email": email,
             "serie": serie,
-            "nota": nota
+            "notas": [nota1,nota2,nota3]
     }
+
 
     alunos.append(aluno)
 
     return alunos
 
-print (cadastraraluno("Miguel", "miguel@gmail.com", "2B", "10"))
+print (cadastraraluno("Miguel", "miguel@gmail.com", "2B", 10,8,9))
